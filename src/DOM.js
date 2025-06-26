33a0ad03c1024e24f78ebf4b4d6a515bd50c1852
Nournouas <171356831+Nournouas@users.noreply.github.com>
2025-06-26 16:24:22 +0100

    added basic interface and CSS file

--- src/DOM.js
+++ src/DOM.js
@@ -1,3 +1,57 @@
-const DOMHandler = (function(){
+import todoLogo from "./to-do-logo.jpg"
+import userLogo from "./user-logo.png"
+
+export function createDOM(){
+    //header ----
+    const header = document.createElement("header");
+    header.classList.add("header")
+
+    const logoImage = document.createElement("img");
+    logoImage.classList.add("logo")
+    logoImage.src = todoLogo;
+
+    const accountImage = document.createElement("img");
+    accountImage.classList.add("accImage")
+    accountImage.src = userLogo;
+
+    header.appendChild(logoImage)
+    header.appendChild(accountImage)
+    //------------
+
+    //-nav-------------------
+    const nav = document.createElement("nav");
+    nav.classList.add("nav");
+
+    const projectHeader = document.createElement("h1");
+    projectHeader.textContent = "Projects:";
+    projectHeader.classList.add("projectHeader");
+    nav.appendChild(projectHeader)
+
+    const projectDiv = document.createElement("div");
+    projectDiv.classList.add("projectsDiv");
+
+    const projectButton = document.createElement("button");
+    projectButton.classList.add("projectButton");
+    projectButton.textContent = "All"
+
+    projectDiv.appendChild(projectButton);
+    nav.appendChild(projectDiv);
     
-})();
+    document.body.appendChild(header);
+    document.body.appendChild(nav);
+    //------------------------
+};
+
+//function to populate projectsDiv
+
+export function populateProjectsDiv(listOfProjects){
+    const projectsDiv = document.querySelector(".projectsDiv");
+    listOfProjects.forEach(project => {
+        const projectButton = document.createElement("button");
+        projectButton.classList.add("projectButton");
+        projectButton.textContent = project.getTitle();
+        projectsDiv.appendChild(projectButton);
+    });
+}
+
+//function to take project and use it to populate the tasks
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import { createItem } from './itemModule.js';
 import { createProject } from './projectModule.js';
+import { createDOM, populateProjectsDiv } from './DOM.js';
 import "./styles.css";
 
 const applicationModule = (function (){
+    const project1 = createProject("projects1", "test", "test", "test", "test")
+    const project2 = createProject("projects2", "test", "test", "test", "test")
+    const project3 = createProject("projects3", "test", "test", "test", "test")
     let allTasks = [];
-    let allProjects = [];
-
+    let allProjects = [project1, project2, project3];
+    createDOM()
+    populateProjectsDiv(allProjects);
 
 })();
 
